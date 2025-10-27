60da025c112cfef377c4c422d66181a93f068a31
alinechana <achana@udesa.edu.ar>
2025-10-27 15:56:09 -0300

    firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import app from "firebase/app";
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCNRO3ga7lauiz60mimDEUjY8KM97-NB6Q",
+    authDomain: "pro3-grupo7-tpfinal.firebaseapp.com",
+    projectId: "pro3-grupo7-tpfinal",
+    storageBucket: "pro3-grupo7-tpfinal.firebasestorage.app",
+    messagingSenderId: "1042416803575",
+    appId: "1:1042416803575:web:570baf8b837dfc4af3bdf2"
+};
+
+
+app.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth();
+export const storage = firebase.storage();
+export const db = app.firestore(); 
